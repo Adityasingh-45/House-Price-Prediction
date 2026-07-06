3a7968c62f3b9efd7ab7e12d5b221d30ddad4622
ADITYA <adityasingh20063945@gmail.com>
2026-07-06 23:12:48 +0530

    Updated app.py for deployment

--- app.py
+++ app.py
@@ -1,15 +1,22 @@
 from flask import Flask, render_template, request
 import pandas as pd
 import joblib
+import os
 
 app = Flask(__name__)
 
+# Base Directory
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
 # Load Saved Pipeline
-model = joblib.load("models/rent_pipeline.pkl")
+model_path = os.path.join(BASE_DIR, "models", "rent_pipeline.pkl")
+model = joblib.load(model_path)
+
 
 @app.route("/")
 def home():
     return render_template("index.html")
+
 
 @app.route("/predict", methods=["POST"])
 def predict():
@@ -20,6 +27,7 @@ def predict():
     bathrooms = int(request.form["bathrooms"])
     area_rate = float(request.form["area_rate"])
 
+    # Create DataFrame
     data = pd.DataFrame({
         "city": [city],
         "area": [area],
@@ -28,12 +36,15 @@ def predict():
         "area_rate": [area_rate]
     })
 
-    prediction = model.predict(data)
+    # Prediction
+    prediction = model.predict(data)[0]
 
     return render_template(
         "index.html",
-        prediction=f"Predicted Rent: ₹ {prediction[0]:,.0f}"
+        prediction=f"Predicted Rent: ₹ {prediction:,.0f}"
     )
 
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
